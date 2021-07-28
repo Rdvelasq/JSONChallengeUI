1d95ef03463ae6030463377317d320b25d9d0b54
Robert <robertdvelasquez72@gmail.com>
2021-07-27 21:15:54 -0400

    Add project files.

---
+++ JSONChallenge/Badge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+//CHALLENGE #2
+{
+
+    public class Badge
+    {
+        public int Id { get; set; }
+        public List<string> ListOfDoors { get; set; }
+        public bool HasSecurityClearance { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Badge()
+        {
+
+        }
+        public Badge(int id, List<string> listOfDoors, bool hasSecurityClearance, int employeeId, DateTime createdOn)
+        {
+            Id = id;
+            ListOfDoors = listOfDoors;
+            HasSecurityClearance = hasSecurityClearance;
+            CreatedOn = createdOn; 
+
+        }
+    }
+}
---
+++ JSONChallenge/Event.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+//CHALLENGE 1
+{
+    public class Event
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Title { get; set; }
+        public int Capacity { get; set; }
+        public decimal PriceOfAdmission { get; set; }
+        public Event()
+        {
+
+        }
+        public Event(DateTime startDate, DateTime endDate, string title, int capacity, decimal priceOfAdmission)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Title = title;
+            Capacity = capacity;
+            PriceOfAdmission = priceOfAdmission;
+             
+                
+        }
+    }
+}
